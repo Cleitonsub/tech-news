0e11afb448a594140360451c6a6b317dd348c818
Cleiton <cleitonrmacedo@gmail.com>
2023-02-01 19:28:08 -0300

    feat: Requisito 7 - Criado funcao search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,16 +1,25 @@
 from tech_news.database import search_news
+from datetime import date as dt
 
 
 # Requisito 6
 def search_by_title(title: str):
     title = search_news({'title': {"$regex": title.lower()}})
+
     result = [(titles['title'], titles['url']) for titles in title]
     return result if result else []
 
 
 # Requisito 7
-def search_by_date(date):
-    """Seu código deve vir aqui"""
+def search_by_date(date: str):
+    try:
+        formated_date = dt.fromisoformat(date).strftime("%d/%m/%Y")
+    except ValueError:
+        raise ValueError('Data inválida')
+
+    news = search_news({'timestamp': formated_date})
+    result = [(new['title'], new['url']) for new in news]
+    return result if result else []
 
 
 # Requisito 8
