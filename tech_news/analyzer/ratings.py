6185140d51ceee52db1c9ae554aaf7961578ba11
Cleiton <cleitonrmacedo@gmail.com>
2023-02-01 20:56:48 -0300

    feat: Requisito 11 - Criado funcao top_5_categories que pega as 5 categorias com maior ocorrencia no banco de dados

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,5 +1,6 @@
 from tech_news.database import find_news
 from operator import itemgetter
+from collections import Counter
 
 
 # Requisito 10
@@ -13,4 +14,9 @@ def top_5_news():
 
 # Requisito 11
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    categories = [new["category"] for new in news]
+    top_categories = Counter(sorted(categories)).most_common(5)
+    # pega o primeiro item tupla
+    result = [category[0] for category in top_categories]
+    return result if result else []
